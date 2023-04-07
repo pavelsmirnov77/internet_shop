fdfe481155274c1b5acb20570354956f7a41e936
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-04-07 13:51:17 +0300

    First push

---
+++ src/test/java/com/example/internet_shop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
